e99ba1589a566358a270b9c472f4dc468681c7fc
Muhammad Ali Nizami <kustalics@gmail.com>
2024-08-13 12:48:03 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('First Appp'),
+          centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
+
