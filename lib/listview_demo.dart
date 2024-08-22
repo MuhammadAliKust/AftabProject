c0baa948431b24df32a51f4e55731a67d8a447ca
Muhammad Ali Nizami <kustalics@gmail.com>
2024-08-22 12:57:30 +0500

    ListView Builder Done

--- lib/listview_demo.dart
+++ lib/listview_demo.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 
-
 class ListViewDemo extends StatelessWidget {
   const ListViewDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("List View Demo"),
-      ),
-      body:const ListTile(
-        leading: Icon(Icons.notifications),
-        title: Text("Notification Title"),
-        subtitle: Text("Notification Sub Title"),
-        trailing: Icon(Icons.delete),
-        tileColor: Colors.blue,
-      ) ,
-    );
+        appBar: AppBar(
+          title: const Text("List View Demo"),
+        ),
+        body: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context, i) {
+          return ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text("Notification Title"),
+            subtitle: Text("Notification Sub Title"),
+            trailing: Icon(Icons.delete),
+            tileColor: Colors.blue,
+          );
+        }));
   }
 }
-
