b578000747f9a4a3ef3d25a675dbf116b7179f73
Muhammad Ali Nizami <kustalics@gmail.com>
2024-08-29 15:23:20 +0500

    Tabbar Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aftab_project/grid_view_demo.dart';
+import 'package:aftab_project/tabbar_view.dart';
 import 'package:flutter/material.dart';
 
 import 'listview_demo.dart';
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GridViewDemo(),
+      home: TabbarViewDemo(),
     );
   }
 }
